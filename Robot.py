4059713ce9ab3519f604154afc26edbad20dbd9e
MariaVinog <maria.vinog@gmail.com>
2016-06-12 15:46:48 +0300

    add more comments

--- Robot.py
+++ Robot.py
@@ -1,87 +1,101 @@
-# -*- coding: utf-8 -*-
 import random
 import sys
 import math
-from numpy import *
-#new
+
 class Robot():
 
-    def __init__(self, Id, IsStatic, IsChargable, x, y, NumOfRobots):
-        self.Id = Id
-        self.x = x
-        self.y = y
-        self.IsChargable = IsChargable 
-        self.NumOfRobots = NumOfRobots
-        self.IsStatic = IsStatic
-        self.Battery = 100.0
-        self.NextStep = -1
-        self.Step = 7
-        self.LastStepWhite = False
-        self.MyNeighbor = -1
-        self.GussLocal = [500,500]
-        self.AllMessage = [None]*(self.NumOfRobots+3)
-        self.indexOfNeighbors=[]
-        self.sendMessage = False                               
-        self.message = [self.Id, self.IsStatic, self.IsChargable, self.x, self.y]
-        self.SendMessage = False
+    def __init__(self, Id, IsStatic, IsChargable, x, y, NumOfBots):
+        self.Id = Id #the robot ID
+        self.x = x #coordinate x of the robot(location)
+        self.y = y #coordinate y of the robot(location)
+        self.IsChargable = IsChargable #True - robot on gray or white
+        self.NumOfBots = NumOfBots #number of all robots
+        self.Battery = 100.0 #robot's battery full
+        self.IsStatic = IsStatic #True - static robot  
+        self.NextStep = -1 #the next step of the robot
+        self.Step = 7 #the last step of the robot
+        self.LastStepWhite = False #the last step on white of the robot 
+        self.MyNeighbor = -1 #current nearest robot
+        self.GussLocal = [500,500] #guess location
+        self.TotalMessages = [None]*(self.NumOfBots+3) #all messages sent that were sent
+        self.IndexSent=[] #the robot from which the message was sent
+        self.Sender = False #thr robots that sends a message                              
+        self.MSG = [self.Id, self.IsStatic, self.IsChargable, self.x, self.y] #contents of the message
+        
     
     def BatteryStatus(self):
-        if(self.IsChargable == 0): #in light
-            self.Battery = min(self.Battery+0.5, 100)  
+        #rechargeable battery - robot in light
+        #battery running low - robot in gray
+        if(self.IsChargable == 0):
+            self.Battery = min(self.Battery+1, 100)  
         else:
-            self.Battery = max(self.Battery-0.5, 0)
+            self.Battery = max(self.Battery-1, 0)
     
-    def Move(self):
+    def StartMoving(self):
+        #the function returns a number between 0-3 or 5
+        # 0 - step right
+        # 1 - step left
+        # 2 - step up
+        # 3 - step down
+        # 5 - robot dead
         if(self.Battery == 0):#dead
             return 5
         self.BatteryStatus() #update battery
         if(self.IsChargable == 0):
+            #robot in light do random move.
             self.NextStep = -1
             self.LastStepWhite = True
             Rand = int(random.random()*100)+1
             if(Rand < 60):
+                #looking for light at neighbors
                 self.Step = self.GoToClosestNeighbor()
                 return self.Step
             else:
+                #do random move
                 self.Step = int(random.random()*4)
                 return self.Step
         else:
+            #robot is not in light - look if last step was light
             if(self.LastStepWhite):
                 return self.GoBack()
             else:
+                #looking for light at neighbors
                 self.Step = self.GoToClosestNeighbor()
                 return self.Step
 
-    def GoToClosestNeighbor(self): #white area.
-        MinimumI = -1
-        minDist = sys.maxsize
-        for i in self.indexOfNeighbors:
-            LastMessage = self.AllMessage[i]
-            if((LastMessage[0][2] == 0) and (LastMessage[1] < minDist)):
-                minDist = LastMessage[1]
-                MinimumI = i
+    def GoToClosestNeighbor(self):
+        #looking for light at neighbors
+        index = -1
+        dist = sys.maxsize
+        for i in self.IndexSent:
+            LastMessage = self.TotalMessages[i]
+            if((LastMessage[0][2] == 0) and (LastMessage[1] < dist)):
+                dist = LastMessage[1]
+                index = i
 
-        if (MinimumI == -1 ):#no neighbors in the light
-            rand = int(random.random()*4)
+        if (index == -1 ):
+            #no neighbors in the light do random move
+            Rand = int(random.random()*4)
             if(self.NextStep == -1):
-                self.NextStep = rand
-                return rand
+                self.NextStep = Rand
+                return Rand
             else:
                 return self.NextStep
 
-        else:#neighbors in light - go to him
-            ClosestNeighbor = self.AllMessage[MinimumI]
+        else:
+            #neighbors in light - go to him
+            ClosestNeighbor = self.TotalMessages[index]
             NewX = self.x - ClosestNeighbor[0][3]
             NewY = self.y - ClosestNeighbor[0][4]
             self.MyNeighbor = ClosestNeighbor
-            if (NewX < 0):
-                return 0
-            elif (NewX > 0):
-                return 1
+            if (NewY > 0):
+                return 3
             elif (NewY < 0):
                 return 2
-            elif (NewY > 0):
-                return 3
+            elif (NewX > 0):
+                return 1
+            elif (NewX < 0):
+                return 0
 
     def GoBack(self):
         if(self.Step == 0):#now in right go to left
@@ -95,40 +109,51 @@ class Robot():
 
     def SendMessage(self):
         if(self.Battery > 0):
-            self.Message = [self.id, self.IsStatic, self.IsChargable, self.x, self.y]
+            #check if robot have battery
+            self.MSG = [self.Id, self.IsStatic, self.IsChargable, self.x, self.y]
             if(self.IsStatic):
-                self.SendMessage = True
-            elif(len(self.indexOfNeighbors) > 3): #moving bot sends to neighbors if knows its location
-                self.SendMessage = True
+                #if static robot - can send message
+                self.Sender = True
+                #if not static - can send only if knows location - have more than 3 neighbors
+            elif(len(self.IndexSent) > 3):
+                self.Sender = True
 
     def GetMessage(self):
-        self.SendMessage = False
+        #robot that receives a message can not send a message
+        self.Sender = False
     
-    def Guess(self):#guess robot location
+    def Guess(self):
+        #guess robot location
         self.Radius = 500
         self.GussLocal = [1000,1000]
         while (self.Radius > 1):
-            GuessPointsAround = self.PointsAround()#4 new guesses around the current guess location
+            #4 new guesses around the current guess location
+            GuessPointsAround = self.PointsAround()
             errors = []
             for g in GuessPointsAround:
-                errors.append(self.RMS(g)) #calculation RMS save in list errors
-            i = errors.index(min(errors)) #error min = best guess location
-            self.GussLocal = GuessPointsAround[i] #save the best guess
+                #calculation RMS save in list errors
+                errors.append(self.RMS(g))
+            #error min = best guess location
+            i = errors.index(min(errors))
+            #save the best guess
+            self.GussLocal = GuessPointsAround[i] 
             self.x = abs(self.GussLocal[0])
             self.y = abs(self.GussLocal[1])
             self.Radius = self.Radius / 2
 
-    def RMS(self, g):#Formula for calculating the approximate point  
+    def RMS(self, gus):
+        #formula for calculating the approximate point  
         ListOfDist =[]
-        for i in self.indexOfNeighbors:
-            LastMessage = self.AllMessage[i]
-            Dist = self.Distance(LastMessage[0][3], LastMessage[0][4], g[0], g[1])
-            realDist = LastMessage[1]
-            Dist = math.pow((Dist-realDist),2)
+        for i in self.IndexSent:
+            LastMessage = self.TotalMessages[i]
+            Dist = self.Distance(LastMessage[0][3], LastMessage[0][4], gus[0], gus[1])
+            realD = LastMessage[1]
+            Dist = math.pow((Dist-realD),2)
             ListOfDist.append(Dist)
         return math.sqrt(sum(ListOfDist)/len(ListOfDist))
 
-    def PointsAround(self):#4 guesses around the current guess
+    def PointsAround(self):
+        #4 guesses around the current guess
         G_x = self.GussLocal[0]
         G_y = self.GussLocal[1]
         return [self.GussLocal, [G_x + self.Radius, G_y], [G_x, G_y + self.Radius],[G_x - self.Radius, G_y], [G_x, G_y - self.Radius]]
