a7c0ff4dc8b393a3cac8757179c8d01b5e021f98
MariaVinog <maria.vinog@gmail.com>
2016-06-12 15:46:02 +0300

    add more comments

--- Simulator.py
+++ Simulator.py
@@ -2,22 +2,21 @@ import Arena
 import random
 import math
 
-
 def main():
-    a = Arena.Arena()
-    a.RandomArena()
-    a.NewRobots(a.StaticBots, True)
-    a.NewRobots(a.MoveBots, False)
-    moveRobot(a)
-    a.root.mainloop()
+    New_Arena = Arena.Arena()
+    New_Arena.Arena()
+    New_Arena.NewRobots(New_Arena.StaticBots, True)
+    New_Arena.NewRobots(New_Arena.MoveBots, False)
+    startmoving(New_Arena)
+    New_Arena.root.mainloop()
 
-
-def moveRobot(arena):
+def startmoving(arena):
+    #move the robot in the GUI and update the location of robot in Matrix.
     while(arena.start):
         for i in arena.ListMoveBot:
-            Step = i[0].Move()
+            Step = i[0].StartMoving()
             if (Step == 0):
-                if (arena.Matrix[i[1] + 1][i[2]] != 2):
+                if (arena.Matrix[i[1] + 1][i[2]] == 0 or arena.Matrix[i[1] + 1][i[2]] == 1):
                     arena.Matrix[i[1]][i[2]] = i[0].IsChargable
                     i[0].IsChargable = arena.Matrix[i[1] + 1][i[2]]
                     arena.Matrix[i[1] + 1][i[2]] = i[0].Id
@@ -27,7 +26,7 @@ def moveRobot(arena):
                     arena.canv.move(i[4], 1, 0)
                     arena.canv.update()
             elif (Step == 1):
-                if (arena.Matrix[i[1] - 1][i[2]] != 2):
+                if (arena.Matrix[i[1] - 1][i[2]] == 0 or arena.Matrix[i[1] - 1][i[2]] == 1):
                     arena.Matrix[i[1]][i[2]] = i[0].IsChargable
                     i[0].IsChargable = arena.Matrix[i[1] - 1][i[2]]
                     arena.Matrix[i[1] - 1][i[2]] = i[0].Id
@@ -37,7 +36,7 @@ def moveRobot(arena):
                     arena.canv.move(i[4], -1, 0)
                     arena.canv.update()
             elif (Step == 2):
-                if (arena.Matrix[i[1]][i[2] + 1] != 2):
+                if (arena.Matrix[i[1]][i[2] + 1] == 0 or arena.Matrix[i[1]][i[2] + 1] == 1):
                     arena.Matrix[i[1]][i[2]] = i[0].IsChargable
                     i[0].IsChargable = arena.Matrix[i[1]][i[2] + 1]
                     arena.Matrix[i[1]][i[2] + 1] = i[0].Id
@@ -47,7 +46,7 @@ def moveRobot(arena):
                     arena.canv.move(i[4], 0, 1)
                     arena.canv.update()
             elif (Step == 3):  # step down
-                if (arena.Matrix[i[1]][i[2] - 1] != 2):
+                if (arena.Matrix[i[1]][i[2] - 1] == 0 or arena.Matrix[i[1]][i[2] - 1] == 1):
                     arena.Matrix[i[1]][i[2]] = i[0].IsChargable
                     i[0].IsChargable = arena.Matrix[i[1]][i[2] - 1]
                     arena.Matrix[i[1]][i[2] - 1] = i[0].Id
@@ -56,38 +55,43 @@ def moveRobot(arena):
                     arena.canv.move(i[3], 0, -1)
                     arena.canv.move(i[4], 0, -1)
                     arena.canv.update()
-
-            if (i[0].Battery == 0):
+                    
+            if int(i[0].Battery == 50):
+                arena.canv.itemconfig(i[3], fill="green")
+            elif (i[0].Battery == 0):
                 arena.canv.itemconfig(i[3], fill="black")
             elif ((i[0].Battery < 50) and (i[0].Battery > 0)):
                 arena.canv.itemconfig(i[3], fill="yellow")
+        SendMSG(arena)
 
-def SendMSG(arena): #bot - get or send mesage.
-    for i in arena.StaticBots:
-        rand = random.randint(0,1)
-        if(rand == 0):
+def SendMSG(arena):
+    #determine which robots send/receive a message
+    for i in arena.ListStaticBot:
+        Rand = int(random.random()*2)
+        if(Rand == 0):
             i[0].GetMessage()
         else:
             i[0].SendMessage()
-    for rob in arena.MoveBots:
-        rand = random.randint(0,1)
-        if (rand ==0):
+    for i in arena.ListMoveBot:
+        Rand = int(random.random()*2)
+        if (Rand == 0):
             i[0].GetMessage()
         else:
             i[0].GetMessage()
     MyAir(arena)
 
-def MyAir(arena): #Check bot recived messages.
+def MyAir(arena): 
+    #Check that all robots recived messages.
     SendBots = []
     ReciveBots = []
     
-    for i in arena.StaticBots:
+    for i in arena.ListStaticBot:
         if i[0].SendMessage:
             SendBots.append(i)
         else:
             ReciveBots.append(i)
 
-    for rob in arena.MoveBots:
+    for i in arena.ListMoveBot:
         if i[0].SendMessage:
             SendBots.append(i)
         else:
@@ -97,29 +101,29 @@ def MyAir(arena): #Check bot recived messages.
         if (len(SendBots) != 0):
             MBot = SendBots[0]
             minDis = RDistance(i,MBot)
-    
             for j in SendBots:
                 dis = RDistance(i,j)
                 if(dis < minDis):
                     MBot = j
                     minDis = dis
             if (minDis < 500):
-                i[0].AllMessages[MBot[0].id] = ([MBot[0].Message ,minDis])#update id 
-                if(MBot[0].id not in i[0].indexOfNeighbors):
-                    i[0].indexOfNeighbors.append(MBot[0].id)#add to neighbors
+                #update id 
+                i[0].TotalMessages[MBot[0].id] = ([MBot[0].MSG ,minDis])
+                if(MBot[0].id not in i[0].IndexSent):
+                    #add to neighbors
+                    i[0].IndexSent.append(MBot[0].id)
                 if not(i[0].IsStatic):
                     i[0].Guess()
 
-
-def RDistance(i,j):#distance between the robots.
+def RDistance(i,j):
+    #distance between the robots.
     x1 = i[1]
     x2 = j[1]
     y1 = i[2]
     y2 = j[2]
     dis = math.sqrt(math.pow(x1-x2,2)+ math.pow(y1-y2,2))
     return dis * random.unifrom(0.8,1.2)
+ 
                   
 if __name__ == "__main__":
     main()
-
-
